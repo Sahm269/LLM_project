f5c2659a4da2d604f8d022af2e4a26e0808b6947
Daniella Rakotondratsimba <daniellark11@gmail.com>
2025-01-31 17:30:31 +0100

    liste course poage

--- app/page/course_list.py
+++ app/page/course_list.py
@@ -1,6 +1,52 @@
 import streamlit as st
+import psycopg2
+import os
+from dotenv import load_dotenv
 
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Fonction pour obtenir la connexion à la base de données
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
+        )
+        return conn
+    except Exception as e:
+        st.error(f"Erreur de connexion à la base de données: {e}")
+        return None
+
+# Fonction pour récupérer les ingrédients depuis la base de données
+def get_ingredients():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT ingredients FROM liste_courses")
+    ingredients_list = cursor.fetchall()  # Récupère tous les résultats
+    
+    # Fermer la connexion
+    cursor.close()
+    conn.close()
+    
+    return ingredients_list
+
+# Page des courses
 def course_list():
-    st.subheader("Liste des courses")
     st.write("Voici votre liste des courses.")
+    
+    ingredients_list = get_ingredients()
+    
+    if ingredients_list:
+        for ingredient in ingredients_list:
+            st.write(f"🍎 {ingredient[0]}")
+    else:
+        st.write("Aucun ingrédient trouvé.")
+
     st.checkbox("Ajouter un nouvel article")
+
+
--- app/page/user.py
+++ app/page/user.py
@@ -9,29 +9,71 @@ from mealplan import mealplan
 from course_list import course_list
 from favoris import favoris
 
-# Titre de la page User
-st.title(f"Bienvenue {st.session_state['user']} !")
+
+# Appliquer le style personnalisé aux headers
+st.markdown("""
+    <style>
+        /* Style global pour les headers */
+        h3 {
+            font-size: 20px;
+            font-family: New Icon;
+            font-weight: 700;
+        }
+            
+        h2 {
+            font-size: 2rem;
+            color: #2a4b47;
+        }
+
+        .welcome-title {
+            font-size: 2.5rem;
+            font-weight: 700;
+            color: #2a4b47;
+            text-align: center;
+            animation: fadeIn 2s ease-out;
+        }
+
+        @keyframes fadeIn {
+            0% { opacity: 0; }
+            100% { opacity: 1; }
+        }
+
+        .user-name {
+            color: #4e7a63;
+            font-size: 3rem;
+            font-weight: bold;
+            animation: nameAnimation 2s ease-out;
+            font-family: New Icon;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Affichage du message de bienvenue
+st.markdown(f"""
+    <h2 class="welcome-title">
+        Bienvenue sur NutriGénie <span class="user-name">{st.session_state['user']}</span> 🍽️!
+    </h2>
+""", unsafe_allow_html=True)
 
 # Définition des onglets horizontaux
 tabs = st.tabs(["🧑‍💼 Informations personnelles ", "🍽️ Meal Plan", "🛒 Liste des courses", "⭐ Favoris"])
 
 # Onglet 1 : Informations personnelles
 with tabs[0]:
-    st.header("Informations personnelles")
+    st.markdown('<h3 class="stHeader">🧑‍💼 Informations personnelles</h3>', unsafe_allow_html=True)
     info_perso()  # Charger la page `info_perso.py`
 
 # Onglet 2 : Meal Plan
 with tabs[1]:
-    st.header("Meal Plan")
+    st.markdown('<h3 class="stHeader">🍽️ Meal Plan</h3>', unsafe_allow_html=True)
     mealplan()  # Charger la page `mealplan.py`
 
 # Onglet 3 : Liste des courses
 with tabs[2]:
-    st.header("Liste des courses")
+    st.markdown('<h3 class="stHeader">🛒 Liste des courses</h3>', unsafe_allow_html=True)
     course_list()  # Charger la page `course_list.py`
 
 # Onglet 4 : Favoris
 with tabs[3]:
-    st.header("Favoris")
+    st.markdown('<h3 class="stHeader">⭐ Favoris</h3>', unsafe_allow_html=True)
     favoris()  # Charger la page `favoris.py`
-
